67979b2903a3e2449b26a7582c8c1742b91cb547
dzagozdzon <zagozdzond@gmail.com>
2018-08-08 20:48:22 +0200

    Improve controllers, add validations and flash messages

--- config/routes.rb
+++ config/routes.rb
@@ -63,7 +63,7 @@ Rails.application.routes.draw do
   
   resources :books
   resources :authors
-  resources :category
+  resources :categories
   resources :publishers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
